5579fc22b791d9725290bcb6cef951e08fec3ce2
Nico Lenczewski <Nico.Lenczewski@stud.hs-flensburg.de>
2020-12-06 22:17:49 +0100

    Woche49

---
+++ Business.Model/BusinessObjects/Attempt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De.HsFlensburg.ClientApp049.Business.Model.BusinessObjects
+{
+    public class Attempt
+    {
+        public DateTime AttemptDate { get; set; }
+    
+        public Boolean Success { get; set; }
+    
+    }
+}
---
+++ Business.Model/BusinessObjects/LearningCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De.HsFlensburg.ClientApp049.Business.Model.BusinessObjects
+{
+    public class LearningCard
+    {
+        public String Question { get; set; }
+
+        public String Answer { get; set; }
+
+        public int Box { get; set; }
+
+    }
+}
